76a03c16dfec3828c51ac777d96c14a097b8d28f
codedghost2 <codedghost2@gmail.com>
2019-10-20 16:55:13 +0100

    Initial commit

---
+++ CoreCodedChatbot.Api/CoreCodedChatbot.Api/Package.cs
@@ -0,0 +1,28 @@
+﻿using CoreCodedChatbot.Library.Models.Data;
+using Microsoft.Extensions.DependencyInjection;
+using TwitchLib.Api;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+
+namespace CoreCodedChatbot.Api
+{
+    public static class Package
+    {
+        public static IServiceCollection AddTwitchServices(this IServiceCollection services, ConfigModel config)
+        {
+            var api = new TwitchAPI();
+            api.Settings.AccessToken = config.ChatbotAccessToken;
+
+            // TODO: Remove the need for the playlist service to talk directly in chat when opening the playlist.
+            var creds = new ConnectionCredentials(config.ChatbotNick, config.ChatbotPass);
+            var client = new TwitchClient();
+            client.Initialize(creds, config.StreamerChannel);
+            client.Connect();
+
+            services.AddSingleton(api);
+            services.AddSingleton(client);
+
+            return services;
+        }
+    }
+}
---
+++ CoreCodedChatbot.Api/CoreCodedChatbot.Api/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreCodedChatbot.Database.Context;
+using CoreCodedChatbot.Database.Context.Interfaces;
+using CoreCodedChatbot.Library;
+using CoreCodedChatbot.Library.Interfaces.Services;
+using CoreCodedChatbot.Library.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using TwitchLib.Api;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+
+namespace CoreCodedChatbot.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var config = new ConfigService().GetConfig();
+
+            services.AddOptions();
+            services.AddMemoryCache();
+
+            services.AddAuthentication(op =>
+                {
+                    op.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    op.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    op.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.ApiSecretSymmetricKey)),
+                        ValidateIssuer = true,
+                        ValidIssuer = config.ApiValidIssuer,
+                        ValidateAudience = true,
+                        ValidAudience = config.ApiValidAudience
+                    };
+                });
+
+            services
+                .AddTwitchServices(config)
+                .AddLibraryServices();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
