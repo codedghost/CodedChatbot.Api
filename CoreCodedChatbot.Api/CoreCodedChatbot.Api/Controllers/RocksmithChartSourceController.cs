57032a1f86a86e9904fbcc08b614df93d562a553
codedghost2 <codedghost2@gmail.com>
2020-03-22 01:12:49 +0000

    Securing endpoint

--- CoreCodedChatbot.Api/CoreCodedChatbot.Api/Controllers/RocksmithChartSourceController.cs
+++ CoreCodedChatbot.Api/CoreCodedChatbot.Api/Controllers/RocksmithChartSourceController.cs
@@ -1,8 +1,12 @@
 ﻿using CoreCodedChatbot.ApiApplication.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCodedChatbot.Api.Controllers
 {
+    [Route("DevOps/[action]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class RocksmithChartSourceController : Controller
     {
         private readonly IRocksmithChartSourceService _rocksmithChartSourceService;
