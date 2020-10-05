dd5c9938445c35e5603c47e3c756505ceaba3cca
codedghost2 <codedghost2@gmail.com>
2020-10-06 00:53:57 +0100

    Adding in Background Song check API enpoints

---
+++ CoreCodedChatbot.Api/CoreCodedChatbot.ApiApplication/Interfaces/Services/IClientTriggerService.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCodedChatbot.ApiApplication.Interfaces.Services
+{
+    public interface IClientTriggerService
+    {
+        void TriggerSongCheck(string username);
+        void SendSongToChat(string username, string title, string artist, string url);
+    }
+}
