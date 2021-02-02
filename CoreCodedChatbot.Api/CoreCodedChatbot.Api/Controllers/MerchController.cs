5207572e7fc0c3c5fdecb8304ae67d88e2d388ac
codedghost2 <codedghost2@gmail.com>
2021-02-02 21:42:20 +0000

    Adding Merch Controller

---
+++ CoreCodedChatbot.Api/CoreCodedChatbot.Api/Controllers/MerchController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoreCodedChatbot.Api.Controllers
+{
+    [Route("Merch/[action]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class MerchController : Controller
+    {
+        private readonly ILogger<MerchController> _logger;
+
+        public MerchController(
+            ILogger<MerchController> logger
+            )
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
