e8153f6418149594129e0e375ff283b3fdac36bd
codedghost2 <codedghost2@gmail.com>
2023-05-19 16:23:29 +0100

    Changing Update to plain GET endpoint

--- CoreCodedChatbot.Api/CoreCodedChatbot.Api/Controllers/CounterController.cs
+++ CoreCodedChatbot.Api/CoreCodedChatbot.Api/Controllers/CounterController.cs
@@ -86,8 +86,8 @@ namespace CoreCodedChatbot.Api.Controllers
             }
         }
 
-        [HttpPost]
-        public async Task<IActionResult> UpdateCounter([FromQuery] string counterName)
+        [HttpGet]
+        public async Task<IActionResult> UpdateCounter(string counterName)
         {
             try
             {
